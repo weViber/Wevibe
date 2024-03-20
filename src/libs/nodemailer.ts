a1ed5440328ed171baa1e8f0eef0e65fa202c066
Kun Woo Kim <kimkuns98@gmail.com>
2024-03-20 15:27:07 +0900

    test

--- src/libs/nodemailer.ts
+++ src/libs/nodemailer.ts
@@ -25,13 +25,21 @@ if (
 let transporter = nodemailer.createTransport({
   service: process.env.SYSTEM_EMAIL_SERVICE,
   host: process.env.SYSTEM_EMAIL_HOST,
-  port: 465,
+  port: 587,
   // port: parseInt(process.env.SYSTEM_EMAIL_PORT, 10),
-  secure: true,
+  secure: false,
   auth: {
     user: process.env.SYSTEM_EMAIL_SENDER,
     pass: process.env.SYSTEM_EMAIL_APPPASS,
   },
+});
+
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
 });
 
 export async function verifyEmail({ email, id }: VerifyEmailProps) {
